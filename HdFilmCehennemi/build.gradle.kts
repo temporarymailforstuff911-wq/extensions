125c2ef5120f42720f433376373f25262a8a1836
ordinaryuser <temporarymailforstuff911@gmail.com>
2026-08-25 21:07:22 +0400

    Update build.gradle.kts

--- HdFilmCehennemi/build.gradle.kts
+++ HdFilmCehennemi/build.gradle.kts
@@ -1,3 +1,28 @@
 plugins {
-    id("cloudstream-plugin")
+    id("com.android.library")
+    id("kotlin-android")
+}
+
+android {
+    namespace = "com.kraptor"
+    compileSdk = 34
+
+    defaultConfig {
+        minSdk = 21
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+dependencies {
+    // Cloudstream əsas kitabxanası
+    val cloudstream by configurations
+    cloudstream("com.lagradost:cloudstream3:pre-release")
 }
